6d15eac88edf502ad3767d05ff9d1612ad6b269a
sriram <sriram4contact@gmail.com>
2019-01-22 12:35:26 +0800

    Second commit

--- PixelSum.cpp
+++ PixelSum.cpp
@@ -15,6 +15,7 @@ PixelSum::PixelSum(const unsigned char* buffer, int xWidth, int xHeight) {
 	nonZeroIntegralImage = new int[Width * Height];
 	computeIntegralImages();
 }
+
 PixelSum::PixelSum(const PixelSum& Input) {
 	image = Input.image;
 	Width = Input.Width;
@@ -35,39 +36,28 @@ PixelSum::~PixelSum() {
 	// TODO Auto-generated destructor stub
 }
 
-//void imgResizeBuffer(Image *src, int width, int height)
-//{
-//	src->xWidth = width;
-//	src->xHeight = height;
-//	memset(src->buffer,0,sizeof(float)*src->xWidth*src->xHeight);
-//}
-
-//PixelSum& PixelSum::operator=(const PixelSum& ImageIP) {
-//	PixelSum ImageOP(ImageIP.ImageBuff.buffer,ImageIP.ImageBuff.xWidth,ImageIP.ImageBuff.xHeight);
-////	ImageOP.ImageBuff.buffer = ImageIP.ImageBuff.buffer;
-////	ImageOP.ImageBuff.xHeight = ImageIP.ImageBuff.xHeight;
-////	ImageOP.ImageBuff.xWidth = ImageIP.ImageBuff.xWidth;
-//	return ImageOP;
-//}
-
-
+PixelSum& PixelSum::operator=(const PixelSum& ImageIP) {
+	PixelSum ImageOP(ImageIP.image, ImageIP.Width, ImageIP.Height);
+	return ImageOP;
+}
 
 void PixelSum::computeIntegralImages() {
 	for (int Integralrow = 0; Integralrow < Height; Integralrow++) {
 		for (int Integralcol = 0; Integralcol < Width; Integralcol++) {
 			unsigned int sum = 0;
 			unsigned int nonZeroPixelCount = 0;
-			for (int row = 0; row < Integralrow; row++)
-				for (int col = 0; col < Integralcol; col++){
+			for (int row = 0; row <= Integralrow; row++)
+				for (int col = 0; col <= Integralcol; col++) {
 					sum += *(image + row * Width + col);
-					if (*(image + row * Width + col) != 0)
-					{
+					if (*(image + row * Width + col) != 0) {
 						nonZeroPixelCount++;
 					}
 				}
 			*(integralImage + Integralrow * Width + Integralcol) = (int) sum;
-			*(nonZeroIntegralImage + Integralrow * Width + Integralcol) = (int) nonZeroPixelCount;
-			cout << *(integralImage + Integralrow * Width + Integralcol) << ", " << flush;
+			*(nonZeroIntegralImage + Integralrow * Width + Integralcol) =
+					(int) nonZeroPixelCount;
+			cout << *(integralImage + Integralrow * Width + Integralcol) << ", "
+					<< flush;
 		}
 		cout << endl << flush;
 	}
@@ -75,6 +65,10 @@ void PixelSum::computeIntegralImages() {
 }
 
 unsigned int PixelSum::getPixelSum(int x0, int y0, int x1, int y1) const {
+	if (!(x0 > 0 && x0 < Width && y0 > 0 && y0 < Height && x1 > 0 && x1 < Width
+			&& y1 > 0 && y1 < Height))
+		return 0;
+
 	if (x0 > x1) {
 		int temp = x0;
 		x0 = x1;
@@ -86,6 +80,17 @@ unsigned int PixelSum::getPixelSum(int x0, int y0, int x1, int y1) const {
 		y0 = y1;
 		y1 = temp;
 	}
+	x0 = (x0 > 0 ? x0 : 0);
+	y0 = (y0 > 0 ? y0 : 0);
+	x1 = (x1 < Width ? x1 : Width - 1);
+	y1 = (y1 < Height ? y1 : Height - 1);
+	if (x0 == x1 && y0 == y1) {
+		if (x0 > 0 && x0 < Width && y0 > 0 && y0 < Height)
+			return (*(image + y0 * Width + x0));
+		else
+			return 0;
+	}
+
 	int a = *(integralImage + y0 * Width + x0);
 	int b = *(integralImage + y0 * Width + x1);
 	int c = *(integralImage + y1 * Width + x1);
@@ -93,13 +98,13 @@ unsigned int PixelSum::getPixelSum(int x0, int y0, int x1, int y1) const {
 	unsigned int sum = c - b - d + a;
 	return sum;
 }
-double PixelSum::getPixelAverage(int x0, int y0, int x1, int y1) const {
-	unsigned int sum = getPixelSum(x0, y0, x1, y1);
-	double average = sum / ((x1 - x0) * (y1 - y0));
-	return average;
-}
 
-int PixelSum::getNonZeroCount(int x0, int y0, int x1, int y1) const {
+double PixelSum::getPixelAverage(int x0, int y0, int x1, int y1) const {
+	if (!(x0 > 0 && x0 < Width && y0 > 0 && y0 < Height && x1 > 0 && x1 < Width
+			&& y1 > 0 && y1 < Height))
+		return 0;
+	unsigned int sum = getPixelSum(x0, y0, x1, y1);
+
 	if (x0 > x1) {
 		int temp = x0;
 		x0 = x1;
@@ -111,22 +116,67 @@ int PixelSum::getNonZeroCount(int x0, int y0, int x1, int y1) const {
 		y0 = y1;
 		y1 = temp;
 	}
+	double denominator = 0;
+	if (x0 == x1 || y0 == y1) {
+		if (x0 > 0 && x0 < Width && y0 > 0 && y0 < Height)
+			denominator = 1;
+		else
+			return 0;
+	} else
+		denominator = (double) ((abs(x1 - x0) + 1) * (abs(y1 - y0) + 1));
+	if (sum == 0 && denominator == 0)
+		return 0;
+	double average = (double) sum / denominator;
+	return average;
+}
+
+int PixelSum::getNonZeroCount(int x0, int y0, int x1, int y1) const {
+	if (!(x0 > 0 && x0 < Width && y0 > 0 && y0 < Height && x1 > 0 && x1 < Width
+			&& y1 > 0 && y1 < Height))
+		return 0;
+
+	if (x0 > x1) {
+		int temp = x0;
+		x0 = x1;
+		x1 = temp;
+	}
+
+	if (y0 > y1) {
+		int temp = y0;
+		y0 = y1;
+		y1 = temp;
+	}
+	x0 = (x0 > 0 ? x0 : 0);
+	y0 = (y0 > 0 ? y0 : 0);
+	x1 = (x1 < Width ? x1 : Width - 1);
+	y1 = (y1 < Height ? y1 : Height - 1);
+	if (x0 == x1 && y0 == y1) {
+		if (x0 > 0 && x0 < Width && y0 > 0 && y0 < Height)
+			return 1;
+		else
+			return 0;
+	}
+
 	int a = *(nonZeroIntegralImage + y0 * Width + x0);
 	int b = *(nonZeroIntegralImage + y0 * Width + x1);
-	int c = *(nonZeroIntegralImage + y1 * Width + x1);
-	int d = *(nonZeroIntegralImage + y1 * Width + x0);
+	int c = *(nonZeroIntegralImage + (y1 < Height ? y1 : Height - 1) * Width
+			+ (x1 < Width ? x1 : Width - 1));
+	int d = *(nonZeroIntegralImage + (y1 < Height ? y1 : Height - 1) * Width
+			+ (x0 > 0 ? x0 : 0));
+
+	cout << a << " " << b << " " << c << " " << d << endl << flush;
 	unsigned int sum = c - b - d + a;
 	return sum;
 }
+
 double PixelSum::getNonZeroAverage(int x0, int y0, int x1, int y1) const {
-	int sum = getNonZeroCount(x0, y0, x1, y1);
-	double average = sum / ((x1 - x0) * (y1 - y0));
+	if (!(x0 > 0 && x0 < Width && y0 > 0 && y0 < Height && x1 > 0 && x1 < Width
+			&& y1 > 0 && y1 < Height))
+		return 0;
+	int nonZeroCount = getNonZeroCount(x0, y0, x1, y1);
+	int sum = getPixelSum(x0, y0, x1, y1);
+	if (nonZeroCount == 0 && sum == 0)
+		return 0;
+	double average = (double) sum / (double) nonZeroCount;
 	return average;
 }
-//
-//int PixelSum::getNonZeroCount(int x0, int y0, int x1, int y1) const {
-// return 0;
-//}
-//double PixelSum::getNonZeroAverage(int x0, int y0, int x1, int y1) const {
-// return 0;
-//}
--- main.cpp
+++ main.cpp
@@ -11,24 +11,30 @@
 
 int main(int argc, char **argv) {
 
-unsigned char buffer[100] = {0,0,0,0,5,78,4,2,43,2,
+unsigned char buffer[100] = {
+		0,0,0,0,5,78,4,2,43,2,
 		1,2,3,43,5,78,4,2,43,2,
 		1,2,3,43,5,78,4,2,43,2,
 		1,2,3,43,5,78,4,2,43,2,
 		1,2,3,43,5,78,4,2,43,2,
 		1,2,3,43,5,78,4,2,43,2,
-		1,2,3,43,5,78,4,2,43,2,
+		1,2,3,43,5,0,4,2,43,2,
 		1,2,3,43,5,78,4,2,43,2,
 		1,2,3,43,5,78,4,2,43,2,
 		1,2,3,43,5,78,4,2,43,2};
 
 PixelSum PixelObj(buffer,10,10);
+int x0 = 9;
+int y0 = 9;
+int x1 = 10;
+int y1 = 10;
 
-cout << "Sum = " << PixelObj.getPixelSum(0,0,1,2)<<endl<<flush;
-cout << "Average = " <<  PixelObj.getPixelAverage(0,0,1,2)<<endl<<flush;
 
-cout << "NonZero = " << PixelObj.getNonZeroCount(0,0,1,2)<<endl<<flush;
-cout << "NonZero Average = " <<  PixelObj.getPixelAverage(0,0,1,2)<<endl<<flush;
+cout << "Sum = " << PixelObj.getPixelSum(x0,y0,x1,y1)<<endl<<flush;
+cout << "Average = " <<  PixelObj.getPixelAverage(x0,y0,x1,y1)<<endl<<flush;
+
+cout << "NonZero = " << PixelObj.getNonZeroCount(x0,y0,x1,y1)<<endl<<flush;
+cout << "NonZero Average = " << PixelObj.getNonZeroAverage(x0,y0,x1,y1)<<endl<<flush;
 
 return 0;
 }
